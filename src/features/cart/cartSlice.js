4f38f38765bc6f08cec8656e13df1f9da8467e32
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-02-13 08:17:12 +0200

    Modeling the Cart State

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItem(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity + item.unitPrice;
+    },
+    decreaseItem(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity + item.unitPrice;
+    },
+    cleareCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const { addItem, deleteItem, increaseItem, decreaseItem, cleareCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/users/userSlice';
+import cartRducer from "./features/cart/cartSlice"
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartRducer,
   },
 });
 
