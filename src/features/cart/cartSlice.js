bce460139e66728dfffbcb8a11fabc3257d2f9e3
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-02-13 17:58:37 +0200

    Building the Cart Page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -35,6 +35,8 @@ export const { addItem, deleteItem, increaseItem, decreaseItem, cleareCart } =
 
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
